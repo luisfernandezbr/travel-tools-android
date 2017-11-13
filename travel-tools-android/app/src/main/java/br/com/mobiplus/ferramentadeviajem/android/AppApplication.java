d10fce41480e66ba5616ed36450c0d95221b81ab
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2017-11-13 12:45:32 -0200

    Adding configurations to work with Fabric Crashlytics and Answers

---
+++ travel-tools-android/app/src/main/java/br/com/mobiplus/ferramentadeviajem/android/AppApplication.java
@@ -0,0 +1,21 @@
+package br.com.mobiplus.ferramentadeviajem.android;
+
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by luisfernandez on 13/11/17.
+ */
+
+public class AppApplication extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());
+    }
+}
